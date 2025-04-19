5f41b87fafc59f4ca48d1b68ef53b4e7f758d0ae
Marcelo Wagner Viana <marcelowagnerviana@MacBook-Air-de-Marcelo-2.local>
2025-04-19 19:54:15 -0300

    Finaliza app.py com correções finais

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-From flask import Flask, request, send_from_directory, make_response
+from flask import Flask, request, send_from_directory, make_response
 from flask_cors import CORS
 import yagmail
 import os
